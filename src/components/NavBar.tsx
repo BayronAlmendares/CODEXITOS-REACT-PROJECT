22dcb4bd5069d52d2c60f8909d33c989708904a6
Bayron-Almendares <ing.bayronalmendares@gmail.com>
2023-04-08 13:58:33 -0600

    color mode switch added

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,11 +1,12 @@
-import { HStack, Image, Text } from "@chakra-ui/react";
+import { HStack, Image } from "@chakra-ui/react";
 import logoApp from "../assets/logo.webp";
+import { ColorModeSwitch } from "./ColorModeSwitch";
 
 export const NavBar = () => {
   return (
-    <HStack>
+    <HStack justifyContent="space-between" padding="10px">
       <Image src={logoApp} boxSize="60px" />
-      <Text>NavBar</Text>
+      <ColorModeSwitch />
     </HStack>
   );
 };
