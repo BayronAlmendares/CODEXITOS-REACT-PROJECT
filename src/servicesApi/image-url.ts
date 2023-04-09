c3a95c1d708b7504ad8ffd34f4ed32c3ee7242ac
Bayron-Almendares <ing.bayronalmendares@gmail.com>
2023-04-08 18:06:06 -0600

    optimization of images.

---
+++ src/servicesApi/image-url.ts
@@ -0,0 +1,7 @@
+//funcion para recortar imagen a 600 por 400 px
+const getFastDownloadImageUrl = (url: string) => {
+  const index = url.indexOf("media/") + "media/".length;
+  return url.slice(0, index) + "crop/600/400/" + url.slice(index);
+};
+
+export default getFastDownloadImageUrl;
