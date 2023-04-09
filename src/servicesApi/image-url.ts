ede62302573b3ea4029459823fea00bc9676be3e
Bayron-Almendares <ing.bayronalmendares@gmail.com>
2023-04-09 10:32:05 -0600

    Handling no image games.

--- src/servicesApi/image-url.ts
+++ src/servicesApi/image-url.ts
@@ -1,6 +1,8 @@
+import noImage from "../assets/no-image-ph.webp";
+
 //funcion para recortar imagen a 600 por 400 px
 const getFastDownloadImageUrl = (url: string) => {
-  if (!url) return "";
+  if (!url) return noImage;
   const index = url.indexOf("media/") + "media/".length;
   return url.slice(0, index) + "crop/600/400/" + url.slice(index);
 };
