5c9ccbf37c0135162ad3c282e315f50f85f11f96
Bayron-Almendares <ing.bayronalmendares@gmail.com>
2023-04-08 21:35:09 -0600

    Display genres

--- src/components/GenreList.tsx
+++ src/components/GenreList.tsx
@@ -1,14 +1,25 @@
+import { HStack, Image, List, ListItem, Text } from "@chakra-ui/react";
 import useGenres from "../customHooks/useGenres";
+import getFastDownloadImageUrl from "../servicesApi/image-url";
 
 const GenreList = () => {
   const { data } = useGenres();
 
   return (
-    <ul>
+    <List>
       {data.map((genre) => (
-        <li key={genre.id}>{genre.name}</li>
+        <ListItem key={genre.id} paddingY="4px">
+          <HStack>
+            <Image
+              boxSize="32px"
+              borderRadius={8}
+              src={getFastDownloadImageUrl(genre.image_background)}
+            />
+            <Text fontSize="lg">{genre.name}</Text>
+          </HStack>
+        </ListItem>
       ))}
-    </ul>
+    </List>
   );
 };
 
