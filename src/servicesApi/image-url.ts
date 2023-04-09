e6f8735c8e2c161eb0dd3d905af94792675fc4e3
Bayron-Almendares <ing.bayronalmendares@gmail.com>
2023-04-09 10:10:21 -0600

    component sortSelector to sort games better

--- src/servicesApi/image-url.ts
+++ src/servicesApi/image-url.ts
@@ -1,5 +1,6 @@
 //funcion para recortar imagen a 600 por 400 px
 const getFastDownloadImageUrl = (url: string) => {
+  if (!url) return "";
   const index = url.indexOf("media/") + "media/".length;
   return url.slice(0, index) + "crop/600/400/" + url.slice(index);
 };
